e2170f07f11dacec84563cbccc028b1727ec42c0
Bada9595 <baduge9595@gmail.com>
2024-11-04 16:59:53 +0900

    1104

--- 1101/다소니심리상담1/js/main.js
+++ 1101/다소니심리상담1/js/main.js
@@ -27,6 +27,18 @@ var a2 = $('.s2_title h2').offset().top; //제목
 var a3 = $('.s2_title p').offset().top; // 내용
 var a4 = $('.s2_table li').offset().top; // 사각박스
 console.log(a1,a2,a3,a4);
+// #s3
+var b1 = $('.s3_title img').offset().top; // '/'
+var b2 = $('.s3_title h2').offset().top; //제목
+var b3 = $('.s3_title p').offset().top; // 내용
+var b4 = $('.s3_table li').offset().top; // 사각박스
+console.log(b1,b2,b3,b4);
+//#s4
+var c1 = $('.s4_title img').offset().top; // '/'
+var c2 = $('.s4_title h2').offset().top; //제목
+var c3 = $('.s4_title p').offset().top; // 내용
+var c4 = $('.s4_table li').offset().top; // 사각박스
+console.log(c1,c2,c3,c4);
 
 $(window).scroll(function(){
   var sct = $(this).scrollTop();
@@ -56,7 +68,52 @@ $(window).scroll(function(){
     },1200);
   }
 
+  if(b1 < sct + 700){
+    $('.s3_title img').addClass('slide');
+  }
+  if(b2 < sct + 700){
+    $('.s3_title h2').addClass('slide');
+  }
+  if(b3 < sct + 700){
+    $('.s3_title p').addClass('slide');
+  }
+  if(b4 < sct + 700){
+    $('.s3_table li').eq(0).addClass('slide');
+    setTimeout(function(){
+      $('.s3_table li').eq(1).addClass('slide');
+    },300);
+    setTimeout(function(){
+      $('.s3_table li').eq(2).addClass('slide');
+    },600);
+    setTimeout(function(){
+      $('.s3_table li').eq(3).addClass('slide');
+    },900);
+    setTimeout(function(){
+      $('.s3_table li').eq(4).addClass('slide');
+    },1200);
+  }
 
+  if(c1 < sct + 700){
+    $('.s4_title img').addClass('slide');
+  }
+  if(c2 < sct + 700){
+    $('.s4_title h2').addClass('slide');
+  }
+  if(c3 < sct + 700){
+    $('.s4_title p').addClass('slide');
+  }
+  if(c4 < sct + 700){
+    $('.s4_table li').eq(0).addClass('slide');
+    setTimeout(function(){
+      $('.s4_table li').eq(1).addClass('slide');
+    },300);
+    setTimeout(function(){
+      $('.s4_table li').eq(2).addClass('slide');
+    },600);
+    setTimeout(function(){
+      $('.s4_table li').eq(3).addClass('slide');
+    },900);
+  }
 });
 
 
