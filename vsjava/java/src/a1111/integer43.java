276504229eada377a0b73cc84d6254de64fef7b4
Bada9595 <baduge9595@gmail.com>
2024-11-11 17:07:01 +0900

    1111

---
+++ vsjava/java/src/a1111/integer43.java
@@ -0,0 +1,14 @@
+package a1111;
+
+public class integer43 {
+    public static void main(String[] args) {
+        int var1 = 0b1011; //0b binary 2진수를 뜻한다.
+        int var2 = 0206;
+        int var3 = 365;
+        int var4 = 0xb3;
+        System.out.println("var1: " + var1);
+        System.out.println("var2: " + var2);
+        System.out.println("var3: " + var3);
+        System.out.println("var4: " + var4);
+    }
+}
