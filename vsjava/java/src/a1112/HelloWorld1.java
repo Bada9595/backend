e9037baa00a4e2e0804f5155a336e7fa1ffaa09e
Bada9595 <baduge9595@gmail.com>
2024-11-15 17:37:27 +0900

    1115

--- vsjava/java/src/a1112/HelloWorld1.java
+++ vsjava/java/src/a1112/HelloWorld1.java
@@ -8,3 +8,12 @@ public class HelloWorld1 {
         //이스케이프 문자 찾아 문자 적용해보기.
     }
 }
+// 자주 사용되는 이스케이프 문자
+// \\ : 백슬래시 (\) 출력
+// \' : 작은따옴표 (') 출력
+// \" : 큰따옴표 (") 출력
+// \n : 줄 바꿈 (new line)
+// \t : 탭 (tab)
+// \r : 캐리지 리턴 (carriage return)
+// \b : 백스페이스 (backspace)
+// \f : 폼 피드 (form feed, 페이지 넘김
